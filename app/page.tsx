b904d4da40b083b7bc83829b9edcf1802a517fe4
sam06tanb <samirtajra53@gmail.com>
2024-11-05 23:17:33 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
 
-const Home = () => {
-  return <Button>Oi cu</Button>; // Corrigido com 'return'
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="itens-center flex h-full justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
