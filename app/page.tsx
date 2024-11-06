c80b850f05587c5635ccc04ff51088b6f7303bd9
sam06tanb <samirtajra53@gmail.com>
2024-11-05 23:22:18 -0300

    chore: add git hook status

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import { dark } from "@clerk/themes";
 
 const Home = async () => {
   const { userId } = await auth();
